15c8666726c115cdff5a78eaa26663d933fadb6b
selic <selic@DESKTOP-H8R889D>
2021-02-18 22:35:05 +0100

    Copia de ficheros del directorio src/main/java tarea 5

---
+++ src/main/java/org/iesalandalus/programacion/biblioteca/MainApp.java
@@ -0,0 +1,26 @@
+package org.iesalandalus.programacion.biblioteca;
+
+import org.iesalandalus.programacion.biblioteca.mvc.controlador.Controlador;
+import org.iesalandalus.programacion.biblioteca.mvc.modelo.Modelo;
+import org.iesalandalus.programacion.biblioteca.mvc.vista.Vista;
+
+
+public class MainApp {
+
+	public static void main(String[] args) {
+
+		Modelo modelo = new Modelo();
+		Vista vista = new Vista();
+		Controlador controlador = new Controlador(modelo, vista);
+		controlador.comenzar();
+
+	}
+
+}
+
+/*
+Tarea: Biblioteca IES Al-Ándalus
+Profesor: José Ramón Jiménez Reyes
+Alumno: José Antonio Del Rey Martínez
+Curso: Programación, DAM IES ÁL-ANDALUS
+*/
