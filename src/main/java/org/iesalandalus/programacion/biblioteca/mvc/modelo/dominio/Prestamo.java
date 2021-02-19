b3fe9f59b536092b28c1df3fcbda2c8e6b7de51f
selic <selic@DESKTOP-H8R889D>
2021-02-19 22:02:47 +0100

    Clase Prestamo

--- src/main/java/org/iesalandalus/programacion/biblioteca/mvc/modelo/dominio/Prestamo.java
+++ src/main/java/org/iesalandalus/programacion/biblioteca/mvc/modelo/dominio/Prestamo.java
@@ -7,102 +7,85 @@ import java.util.Objects;
 
 public class Prestamo {
 
-	private static final int MAX_DIAS_PRESTAMO = 30;
+	private static final int MAX_DIAS_PRESTAMO = 20;
 	public static final DateTimeFormatter FORMATO_FECHA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-	private LocalDate fechaPrestamo;
-	private LocalDate fechaDevolucion;
+
 	private Alumno alumno;
 	private Libro libro;
+	private LocalDate fechaPrestamo;
+	private LocalDate fechaDevolucion;
 
 	public Prestamo(Alumno alumno, Libro libro, LocalDate fechaPrestamo) {
-
-		if (alumno == null) {
-			throw new NullPointerException("ERROR: El alumno no puede ser nulo.");
-		}
-		if (libro == null) {
-			throw new NullPointerException("ERROR: El libro no puede ser nulo.");
-		}
-		setAlumno(new Alumno(alumno));
-		setLibro(new Libro(libro));
+		setAlumno(alumno);
+		setLibro(libro);
 		setFechaPrestamo(fechaPrestamo);
 	}
 
 	public Prestamo(Prestamo prestamo) {
-
 		if (prestamo == null) {
 			throw new NullPointerException("ERROR: No es posible copiar un préstamo nulo.");
 		}
-		setAlumno(new Alumno(prestamo.getAlumno()));
-		setLibro(new Libro(prestamo.getLibro()));
-		setFechaPrestamo(prestamo.getFechaPrestamo());
+		alumno = prestamo.getAlumno();
+		libro = prestamo.getLibro();
+		fechaPrestamo = prestamo.getFechaPrestamo();
+		fechaDevolucion = prestamo.getFechaDevolucion();
 	}
 
 	public static Prestamo getPrestamoFicticio(Alumno alumno, Libro libro) {
-
-		if (alumno == null) {
-			throw new NullPointerException("ERROR: El alumno no puede ser nulo.");
-		}
-		if (libro == null) {
-			throw new NullPointerException("ERROR: El libro no puede ser nulo.");
-		}
-		alumno = Alumno.getAlumnoFicticio(alumno.getCorreo());
-		libro = Libro.getLibroFicticio(libro.getTitulo(), libro.getAutor());
 		return new Prestamo(alumno, libro, LocalDate.now());
 	}
 
 	public void devolver(LocalDate fechaDevolucion) {
-
-		if (getFechaDevolucion() != null && getFechaDevolucion().isEqual(fechaDevolucion)) {
+		if (this.fechaDevolucion != null) {
 			throw new IllegalArgumentException("ERROR: La devolución ya estaba registrada.");
-		}
-		if (fechaDevolucion == null) {
-			throw new NullPointerException("ERROR: La fecha de devolución no puede ser nula.");
-		}
-		if (fechaDevolucion.isAfter(LocalDate.now())) {
-			throw new IllegalArgumentException("ERROR: La fecha de devolución no puede ser futura.");
-		}
-		if (fechaDevolucion.isBefore(getFechaPrestamo()) || fechaDevolucion.isEqual(getFechaPrestamo())) {
-			throw new IllegalArgumentException(
-					"ERROR: La fecha de devolución debe ser posterior a la fecha de préstamo.");
 		}
 		setFechaDevolucion(fechaDevolucion);
 	}
 
 	public int getPuntos() {
-		int diasPrestamo = 0;
-		float puntosObtenidos = 0f;
-
+		float puntos = 0f;
 		if (fechaDevolucion != null) {
-			diasPrestamo = (int) ChronoUnit.DAYS.between(fechaPrestamo, fechaDevolucion);
+			int diasPrestamo = (int) ChronoUnit.DAYS.between(fechaPrestamo, fechaDevolucion);
 			if (diasPrestamo <= MAX_DIAS_PRESTAMO) {
-				puntosObtenidos = libro.getPuntos() / diasPrestamo;
-			} else {
-				puntosObtenidos = libro.getPuntos() / MAX_DIAS_PRESTAMO;
+				float factorPuntos = (float) MAX_DIAS_PRESTAMO / (diasPrestamo * MAX_DIAS_PRESTAMO);
+				puntos = libro.getPuntos() * factorPuntos;
 			}
 		}
-		return Math.round(puntosObtenidos);
+		return Math.round(puntos);
 	}
 
 	public Alumno getAlumno() {
-		return alumno;
+		return new Alumno(alumno);
 	}
 
 	private void setAlumno(Alumno alumno) {
 		if (alumno == null) {
 			throw new NullPointerException("ERROR: El alumno no puede ser nulo.");
 		}
-		this.alumno = alumno;
+		this.alumno = new Alumno(alumno);
 	}
 
 	public Libro getLibro() {
+		Libro libro = null;
+		if (this.libro instanceof LibroEscrito) {
+			libro = new LibroEscrito((LibroEscrito)this.libro);
+		} else if (this.libro instanceof AudioLibro) {
+			libro = new AudioLibro((AudioLibro)this.libro);
+		}
+		
 		return libro;
 	}
 
 	private void setLibro(Libro libro) {
 		if (libro == null) {
-			throw new NullPointerException("ERROR: El libro no puede ser nulo.");
+			throw new  NullPointerException("ERROR: El libro no puede ser nulo.") ;
 		}
-		this.libro = libro;
+		if (libro instanceof LibroEscrito) {
+			this.libro = new LibroEscrito((LibroEscrito)libro);
+		} else if (this.libro instanceof AudioLibro) {
+			this.libro = new AudioLibro((AudioLibro)libro);
+		}
+		
 	}
 
 	public LocalDate getFechaPrestamo() {
@@ -126,6 +109,10 @@ public class Prestamo {
 	private void setFechaDevolucion(LocalDate fechaDevolucion) {
 		if (fechaDevolucion == null) {
 			throw new NullPointerException("ERROR: La fecha de devolución no puede ser nula.");
+		}
+		if (fechaDevolucion.isBefore(fechaPrestamo) || fechaDevolucion.isEqual(fechaPrestamo)) {
+			throw new IllegalArgumentException(
+					"ERROR: La fecha de devolución debe ser posterior a la fecha de préstamo.");
 		}
 		if (fechaDevolucion.isAfter(LocalDate.now())) {
 			throw new IllegalArgumentException("ERROR: La fecha de devolución no puede ser futura.");
@@ -152,14 +139,15 @@ public class Prestamo {
 
 	@Override
 	public String toString() {
+		String mensaje;
 		if (fechaDevolucion == null) {
-			return String.format("alumno=(%s), libro=(%s), fecha de préstamo=%s, puntos=%d", alumno, libro,
-					fechaPrestamo.format(Prestamo.FORMATO_FECHA), getPuntos());
+			mensaje = String.format("alumno=(%s), libro=(%s), fecha de préstamo=%s, puntos=%s", alumno, libro,
+					fechaPrestamo.format(FORMATO_FECHA), getPuntos());
 		} else {
-			return String.format("alumno=(%s), libro=(%s), fecha de préstamo=%s, fecha de devolución=%s, puntos=%d",
-					alumno, libro, fechaPrestamo.format(Prestamo.FORMATO_FECHA),
-					fechaDevolucion.format(Prestamo.FORMATO_FECHA), getPuntos());
+			mensaje = String.format("alumno=(%s), libro=(%s), fecha de préstamo=%s, fecha de devolución=%s, puntos=%s",
+					alumno, libro, fechaPrestamo.format(FORMATO_FECHA), fechaDevolucion.format(FORMATO_FECHA),
+					getPuntos());
 		}
+		return mensaje;
 	}
-
 }
